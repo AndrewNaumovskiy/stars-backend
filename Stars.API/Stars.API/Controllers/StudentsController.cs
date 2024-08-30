a9d610738eb914679e3d9cb83b696c684b582c40
Naumovskyi Andrii <32496284+AndrewNaumovskiy@users.noreply.github.com>
2024-08-30 21:10:04 +0300

    Add an ability to search in get students by group

--- Stars.API/Stars.API/Controllers/StudentsController.cs
+++ Stars.API/Stars.API/Controllers/StudentsController.cs
@@ -46,18 +46,26 @@ public class StudentsController : ControllerBase
     [HttpGet]
     [EnableCors]
     [Route("group/{id}")]
-    public async Task<ActionResult<ResponseModel<GetStudentsByGroupIdData, IError>>> GetStudentsByGroupId(int id)
+    public async Task<ActionResult<ResponseModel<GetStudentsByGroupIdData, IError>>> GetStudentsByGroupId(int id, [FromQuery]string? search)
     {
         List<StudentDbModel> students = null;
 
         using (var db = await _dbContext.CreateDbContextAsync())
         {
-            students = await db.Students.AsNoTracking()
-                                        .Where(x => x.GroupFk == id)
-                                        .OrderBy(x => x.LastName)
-                                        .ThenBy(x => x.FirstName)
-                                        .ThenBy(x => x.MiddleName)
-                                        .ToListAsync();
+            var query = db.Students.AsNoTracking()
+                                   .Where(x => x.GroupFk == id);
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                query = query.Where(x => x.LastName.Contains(search) ||
+                                         x.FirstName.Contains(search) ||
+                                         x.MiddleName.Contains(search));
+            }
+
+            students = await query.OrderBy(x => x.LastName)
+                                  .ThenBy(x => x.FirstName)
+                                  .ThenBy(x => x.MiddleName)
+                                  .ToListAsync();
         }
 
         return Ok(new ResponseModel<GetStudentsByGroupIdData, IError>()
